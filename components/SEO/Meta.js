24a815c31af0a130305b90bdd893dcb4bc6fa8d3
Batuhan TURGUT <batuhankutahya43@gmail.com>
2022-04-22 23:01:53 +0300

    Initial commit

---
+++ components/SEO/Meta.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Head from 'next/head';
+
+export default function Meta() {
+    return (
+        <Head>
+           /* Primary Meta Tags */
+            <title>Batuhan Turgut Portfolio - Computer Engineering Student</title>
+            <meta charSet="utf-8" />
+            <meta name="title" content="Batuhan Turgut Portfolio - Computer Engineering Student" />
+            <meta name="description"
+                content="Batuhan Turgut's (betaisyn5243) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="author" content="Batuhan Turgut (betaisyn5243)" />
+            <meta name="keywords"
+                content="betaisyn5243, betaisyn5243's portfolio, betaisyn5243 linux, ubuntu portfolio, Batuhan Turgut protfolio,Batuhan Turgut computer, Batuhan Turgut, batuan ubuntu, Batuhan Turgut ubuntu portfolio" />
+            <meta name="robots" content="index, follow" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#E95420" />
+
+            /* Search Engine */
+            <meta name="image" content="images/logos/fevicon.png" />
+            /* Schema.org for Google */
+            <meta itemProp="name" content="Batuhan Turgut Portfolio - Computer Engineering Student" />
+            <meta itemProp="description"
+                content="Batuhan Turgut's (betaisyn5243) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta itemProp="image" content="images/logos/fevicon.png" />
+            /* Twitter */
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Batuhan Turgut Portfolio - Computer Engineering Student" />
+            <meta name="twitter:description"
+                content="Batuhan Turgut's (betaisyn5243) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="twitter:site" content="betaisyn5243" />
+            <meta name="twitter:creator" content="betaisyn5243" />
+            <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
+            /* Open Graph general (Facebook, Pinterest & Google+) */
+            <meta name="og:title" content="Batuhan Turgut Portfolio - Computer Engineering Student" />
+            <meta name="og:description"
+                content="Batuhan Turgut's (betaisyn5243) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS." />
+            <meta name="og:image" content="images/logos/logo_1200.png" />
+           <meta name="og:url" content="http://batuhanturgut.com.tr/" />
+             <meta name="og:site_name" content="Batuhan Turgut Personal Portfolio" />
+            <meta name="og:locale" content="en_IN" />
+            <meta name="og:type" content="website" />
+
+            <link rel="icon" href="images/logos/fevicon.svg" />
+            <link rel="apple-touch-icon" href="images/logos/logo.png" />
+            <link rel="preload" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" as="style" />
+            <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+        </Head>
+    )
+}
---
+++ components/apps/spotify.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spotify() {
+    return (
+        <iframe src="https://open.spotify.com/embed/playlist/7He5Sjj2ddTxja3ndgii0s?si=1d04566ad57141c4" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
+export const displaySpotify = () => {
+    <Spotify> </Spotify>
+}
